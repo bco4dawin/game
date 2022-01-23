462ae02bc3155644085e195cd44d0d3b78a19865
bco4dawin <bco4dawin@gmail.com>
2022-01-23 10:38:59 -0500

    big update

--- game/main.py
+++ game/main.py
@@ -1,49 +1,125 @@
-from pygame import *
+import pygame, math
 from sys import exit
 
-width, height = 800, 400
-init()
-screen = display.set_mode((width, height))
-display.set_caption("Test")
-clock = time.Clock()
-funt = font.Font('Pixeltype.ttf', 50)
+sWidth, sHeight = 1200, 600
+clock = pygame.time.Clock()
 
-back = Surface((800,400))
-back.fill("Black")
-bg = image.load('Assets/CXcV8Yh.png').convert_alpha()
-ground = image.load('Assets/ground.png').convert_alpha()
-txt = funt.render("Test Run", False, "Green")
-enemy = image.load("Assets/enemy.png").convert_alpha()
-enemy_posX = 800
-enemy_posY = 250
-brr = 1
-
-while True:
-    for ev in event.get():
-        if ev.type == QUIT:
-            quit()
+def events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
             exit()
 
-    screen.blit(back, (0,0))
-    screen.blit(bg, (0, -460))
-    screen.blit(ground, (0, 165))
-    screen.blit(txt, (340, 100))
-    enemy_posX -= 2
-    if enemy_posY >= 200 and enemy_posY <= 250 and brr == 0:
-        enemy_posY += 1
-        if enemy_posY >= 250:
-            brr = 1
-    elif enemy_posY <= 250 and enemy_posY >= 200 and brr == 1:
-        enemy_posY -= 1
-        if enemy_posY <= 200:
-            brr = 0
+pygame.init()
+
+screen = pygame.display.set_mode((sWidth, sHeight))
+pygame.display.set_caption("Dungeon Echos")
+
+#font
+font = pygame.font.Font("Pixeltype.ttf", 50)
 
 
-    
+#Player 
+playerArr = []
+for i in range(6):
+    img = pygame.image.load(f'Assets/Adventurer/Individual Sprites/adventurer-run-0{i}.png')
+    rect = img.get_rect()
+    img = pygame.transform.scale(img, (rect.width * 3, rect.height * 3))
+    playerArr.append(img)
+
+idle = pygame.image.load(f'Assets/Adventurer/Individual Sprites/adventurer-idle-00.png')
+rect = idle.get_rect()
+idle = pygame.transform.scale(idle, (rect.width * 3, rect.height * 3))
+
+sheet = pygame.image.load('Assets/wizard.png').convert_alpha()
+cell = []
+
+for i in range(10):
+    w, h = 80, 80
+    rect = pygame.Rect(i * w, 0, w, h)
+    image = pygame.Surface(rect.size).convert()
+    image.blit(sheet, (0,0), rect)
+    alpha = image.get_at((0, 0))
+    image.set_colorkey(alpha)
+    cell.append(image)
+
+
+
+bg = pygame.image.load('Assets/bg.png').convert()
+rect = bg.get_rect()
+bg = pygame.transform.scale(bg, (1200, 600))
+x = 0
+
+runningCount = 0
+hor = 100
+isJump = False
+jumpCount = 10
+pHeight = 2 * (sHeight /3) + 10
+score = 0
+run = 0
+
+while True:
+
+    global obstacles
+    obstacles = []
+    events()
+
+    if run:
+        rel_x = x % bg.get_rect().width
+        screen.blit(bg, (rel_x  - bg.get_rect().width,0))
+        if rel_x < sWidth:
+            screen.blit(bg, (rel_x, 0))
+        x -= 5
         
-    if enemy_posX < -100: 
-        enemy_posX = 800
-    screen.blit(enemy, (enemy_posX, enemy_posY))
+        #Controls
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            if hor > -20:
+                hor -= 6
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            if hor < 1050:
+                hor += 6
+        if not(isJump):
+            if keys[pygame.K_SPACE] or keys[pygame.K_UP] or keys[pygame.K_w]:
+                isJump = True
+        else:
+            if jumpCount >= -10:
+                pHeight -=  jumpCount*2
+                jumpCount -= 1
+            else: 
+                isJump = False
+                jumpCount = 10
+        
+        
+        screen.blit(playerArr[math.floor(runningCount)], (hor, pHeight))
+        if math.floor(runningCount) == 5:
+            runningCount = 0
+        else:
+            runningCount += 0.15
 
-    display.update()
-    clock.tick(60)
+
+        scoreTXT = font.render(f'Score: {math.floor(score)}', True, (255,255,255))
+        screen.blit(scoreTXT, (500, 10))
+        score += 0.25
+
+        
+
+    else:
+
+        screen.blit(bg, (0,0))
+        screen.blit(idle, (hor, pHeight))
+        cover = pygame.Surface((1200, 600))
+        cover.set_alpha(150)
+        cover.fill((0,0,0))
+        screen.blit(cover, (0,0))
+
+        startTXT = font.render("Press [Space] To Start", True, (255,255,255))
+        screen.blit(startTXT, (400, 275))
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            run = 1
+
+
+    pygame.display.update()
+    clock.tick(120)
